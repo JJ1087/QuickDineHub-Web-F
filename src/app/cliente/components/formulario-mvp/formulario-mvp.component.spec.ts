9c86683770358a8131bcdb4ea4c80a239e9869ce
juliogmo <20200775@uthh.edu.mx>
2024-10-23 16:08:50 -0600

    QW-8 propuesta mvp

---
+++ src/app/cliente/components/formulario-mvp/formulario-mvp.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FormularioMvpComponent } from './formulario-mvp.component';
+
+describe('FormularioMvpComponent', () => {
+  let component: FormularioMvpComponent;
+  let fixture: ComponentFixture<FormularioMvpComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [FormularioMvpComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(FormularioMvpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

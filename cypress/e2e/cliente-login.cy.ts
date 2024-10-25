63520b6ec2eedacdf09f2f23472acfe221b1621e
JJ1087 <20200708@uthh.edu.mx>
2024-10-25 12:59:10 -0600

    Pruebas PWA5 cypress

--- cypress/e2e/cliente-login.cy.ts
+++ cypress/e2e/cliente-login.cy.ts
@@ -1,4 +1,5 @@
 describe("Prueba E2E para el flujo de login", () => {
+
     Cypress.on("uncaught:exception", (err, runnable) => {
       if (err.message.includes("Cannot read properties of null")) {
         return false;
@@ -18,6 +19,26 @@ describe("Prueba E2E para el flujo de login", () => {
   
     });
 
-  
+    it("Debe mostrar un mensaje de error con credenciales inválidas", () => {
+      // Visita la página de inicio de sesión
+      cy.visit("/login-clientes");
+    
+      // Ingresa un correo y una contraseña inválidos
+      cy.get('input[id="correo"]').type("correo@incorrecto.com");
+      cy.get('input[id="password"]').type("contraseñaIncorrecta");
+    
+      // Haz clic en el botón de envío
+      cy.get('button[type="submit"]').click({ force: true });
+    
+      // Verifica que el mensaje de error aparezca y sea visible
+      cy.contains("La contraseña o email son incorrectos", { timeout: 10000 }).should("be.visible");
+    });
+    
+   
+    
+    
+    
+    
+    
     
   });
